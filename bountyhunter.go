7d1526c623495f10310bc904076419e59be39bc5
Dylan Leggio <dylan@legg.io>
2020-09-08 00:12:40 -0400

    working stream version

--- bountyhunter.go
+++ bountyhunter.go
@@ -5,10 +5,12 @@ import (
   "flag"
   "fmt"
   "log"
+  "net"
   "net/http"
-  "time"
+  "regexp"
+  "strings"
 
-  "github.com/dlegs/bounty-hunter/subdomains"
+  "github.com/CaliDog/certstream-go"
 )
 
 const (
@@ -21,31 +23,78 @@ var (
 )
 
 func main() {
-  start := time.Now()
   flag.Parse()
-  targets, err := fetchBountyTargets()
+
+  // Fetch bug bounty targets as regexes.
+  // TODO: Fetch every hour.
+  regexes, err := fetchBountyTargets()
   if err != nil {
     log.Fatalf("failed to fetch bounty targets: %v", err)
   }
-  s := subdomains.New()
-  subdomains, err := s.Enumerate(targets)
-  if err != nil {
-    log.Fatalf("failed to enumerate subdomains: %v", err)
+
+  // Kick off certstream.
+  stream, errStream := certstream.CertStreamEventStream(false)
+  for {
+    select {
+    case jq := <-stream:
+      // Skip heartbeat messages.
+      messageType, err := jq.String("message_type")
+      if err != nil {
+        log.Fatalf("failed to decode jq string: %v", err)
+      }
+      if messageType == "heartbeat" {
+        break
+      }
+
+      // Parse domains from cert log.
+      domains, err := jq.Array("data", "leaf_cert", "all_domains")
+      if err != nil {
+        log.Fatalf("failed to decode jq array: %v", err)
+      }
+      // Check if domains match bug bounty target regexes.
+      // TODO: deal with dupes.
+      for _, domain := range domains {
+        for _, regex := range regexes {
+          if regex.MatchString(domain.(string)) {
+            log.Printf("Found domain: %q", domain)
+            if !resolves(domain.(string)) {
+              continue
+            }
+            // TODO: port scan etc.
+          }
+        }
+      }
+
+    case err := <-errStream:
+      log.Print(err)
+    }
   }
-  log.Println(subdomains)
-  log.Println(time.Since(start))
 }
 
-func fetchBountyTargets() ([]string, error) {
+// fetchBountyTargets fetches wildcard domains of bug bounty targets from
+// https://github.com/arkadiyt/bounty-targets-data and return a list of compiled
+// regexes.
+func fetchBountyTargets() ([]*regexp.Regexp, error) {
   res, err := http.Get(targetsURL)
   if err != nil {
     return nil, fmt.Errorf("failed to fetch targets: %v", err)
   }
 
-  targets := []string{}
+  regexes := []*regexp.Regexp{}
   scanner := bufio.NewScanner(res.Body)
   for scanner.Scan() {
-    targets = append(targets, scanner.Text())
+    pattern := regexp.QuoteMeta(scanner.Text())
+    pattern = strings.ReplaceAll(pattern, `\*`, ".*")
+    regex := regexp.MustCompile(pattern)
+    regexes = append(regexes, regex)
   }
-  return targets, nil
+  return regexes, nil
+}
+
+func resolves(domain string) bool {
+  _, err := net.ResolveIPAddr("ip4", domain)
+  if err != nil {
+    return false
+  }
+  return true
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/dlegs/bounty-hunter
 
 go 1.15
+
+require (
+	github.com/CaliDog/certstream-go v0.0.0-20200713031452-eca7997412f1
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jmoiron/jsonq v0.0.0-20150511023944-e874b168d07e // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
