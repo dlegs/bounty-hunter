ffb0b9648a365d897f2c65915b5aa0c2138412e2
Dylan Leggio <dylan@legg.io>
2020-09-08 21:38:15 -0400

    basic storage implementation

---
+++ storage/storage.go
@@ -0,0 +1,70 @@
+// Package storage provides intofaces into an sqlite database for storing
+// found subdomains.
+package storage
+
+import (
+  "fmt"
+  "database/sql"
+
+  _ "github.com/mattn/go-sqlite3"
+)
+
+type Client struct {
+  db *sql.DB
+}
+
+func New(dbName string) (*Client, error) {
+  db, err := sql.Open("sqlite3", dbName)
+  if err != nil {
+    return nil, fmt.Errorf("failed opening db %q: %v", dbName, err)
+  }
+
+  if _, err := db.Exec("CREATE TABLE IF NOT EXISTS domains (domain TEXT PRIMARY KEY)"); err != nil {
+    return nil, fmt.Errorf("failed creating domains table: %v", err)
+  }
+
+  if _, err := db.Exec("CREATE TABLE IF NOT EXISTS subdomains (subdomain TEXT PRIMARY KEY, domain TEXT FOREIGN KEY REFERENCES domains(domain), takeover BOOLEAN)"); err != nil {
+    return nil, fmt.Errorf("failed creating subdomains table: %v", err)
+  }
+
+  if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ports (id INTEGER AUTO_INCREMENT PRIMARY KEY, port INTEGER, subdomain TEXT FOREIGN KEY REFERENCES subdomains(subdomain), protocol TEXT, service TEXT, product TEXT, version TEXT)"); err != nil {
+    return nil, fmt.Errorf("failed creating ports table: %v", err)
+  }
+
+  return &Client{
+    db: db,
+  }, nil
+}
+
+func (c *Client) InsertDomain(domain string) error {
+  statement, err := c.db.Prepare("INSERT IGNORE INTO domains (domain) VALUES (?)")
+  if err != nil {
+    return fmt.Errorf("failed to prepare insert statement: %v", err)
+  }
+  if _, err := statement.Exec(domain); err != nil {
+    return fmt.Errorf("failed to execute insert statement: %v", err)
+  }
+  return nil
+}
+
+func (c *Client) InsertSubdomain(subdomain string, domain string) error {
+  statement, err := c.db.Prepare("INSERT IGNORE INTO subdomains (subdomain, domain) VALUES (?, ?)")
+  if err != nil {
+    return fmt.Errorf("failed to prepare insert statement: %v", err)
+  }
+  if _, err := statement.Exec(subdomain, domain); err != nil {
+    return fmt.Errorf("failed to execute insert statement: %v", err)
+  }
+  return nil
+}
+
+func (c *Client) InsertPort(port int, subdomain string, protocol string, service string, product string, version string) error {
+  statement, err := c.db.Prepare("INSERT INTO ports (port, subdomain, protocol, service, product, version) VALUES (?, ?, ?, ?, ?, ?)")
+  if err != nil {
+    return fmt.Errorf("failed to prepare insert statement: %v", err)
+  }
+  if _, err := statement.Exec(port, subdomain, protocol, service, product, version); err != nil {
+    return fmt.Errorf("failed to execute insert statement: %v", err)
+  }
+  return nil
+}
